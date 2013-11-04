5701dd3c975150329a673af1cf802a731f2ed459
nickdys <nwd5069@psu.edu>
2013-11-04 01:47:40 -0500

    First application files

---
+++ pebble.py
@@ -0,0 +1,10 @@
+# Feed item
+
+class Pebble:
+    def __init__(self, item, school, index):
+        self.item = item
+        self.school = school
+        self.timestamp
+
+        self.x = ???
+        self.y = ???
